5209aeb808b601676d0d6834b17304afc2896c9a
Yajun <yajunwang@deeproute.ai>
2021-06-16 02:17:06 -0700

    fix

--- plaindjango/views/api.py
+++ plaindjango/views/api.py
@@ -350,10 +350,12 @@ def store_direct_message(direct_message):
     print("Connected to:", mysql_connection.get_server_info())
     mysql_cursor = mysql_connection.cursor(buffered=True)
 
+    date = datetime.fromtimestamp(direct_message.created_timestamp)
+    date_object = date.date().isoformat()
     sql = "INSERT ignore INTO asynctask_message (messageid, sender, receiver, type, content, replied, date) VALUES (%s, %s,%s,%s,%s,%s,%s)"
     val = (direct_message.id, direct_message.message_create['sender_id'],
            direct_message.message_create['target']['recipient_id'], direct_message.type,
-           str(direct_message.message_create['message_data']['text']), "no", datetime.fromtimestamp(direct_message.created_timestamp))
+           str(direct_message.message_create['message_data']['text']), "no", date_object)
 
     mysql_cursor.execute(sql, val)
 
